6ee1529c84d9bd043bdec71b1ea9fc9acef0ceea
DavidCueli <dcueli@hotmail.com>
2026-02-12 13:59:13 +0100

    Versioning document and re-orginzing folder temp with changes on .gitignore file

--- temp/MainActivity.java
+++
@@ -1,9 +0,0 @@
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        registerPlugin(CallRecorderPlugin.class);
-    }
-}
